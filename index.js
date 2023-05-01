0f322a1247f0c05dbc7e1bcb0d99b998e2132fe6
Patricio91 <patricioponcini10@gmail.com>
2023-05-01 18:17:20 -0300

    botones funcionales

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let button = document.getElementById("follow-btn");
 let likeBtn = document.getElementById("like-btn");
 let usernameInput = document.getElementById("login-input");
-let loginBtn = document.getElementById("login-btn");
 let usernameDisplay = document.getElementById("username-display");
 let addCommentBtn = document.getElementById("comment-btn");
 let commentInput = document.getElementById("comment-input");
@@ -9,68 +8,82 @@ let comments = document.getElementById("comments");
 
 // FOLLOW AND UNFOLLOW BUTTON
 button.addEventListener("click", function () {
-    if (button.textContent === "Seguir") {
-        button.textContent = "Dejar de seguir";
-        button.style.backgroundColor = "white";
-        button.style.border = "solid lightgray";
-        button.style.color = "red"
+    if (usernameInput.value === "") {
+        return;
     } else {
-        button.textContent = "Seguir";
-        button.style.backgroundColor = "blue";
-        button.style.border = "none";
-        button.style.color = "white"
+        if (button.textContent === "Seguir") {
+            button.textContent = "Dejar de seguir";
+            button.style.backgroundColor = "white";
+            button.style.border = "solid lightgray";
+            button.style.color = "red"
+        } else {
+            button.textContent = "Seguir";
+            button.style.backgroundColor = "blue";
+            button.style.border = "none";
+            button.style.color = "white"
+        }
     }
 });
 
 // LIKE BUTTON (hacer boton para sacar me gusta)
 let likes = 200;
+let liked = false;
 likeBtn.addEventListener("click", function () {
-    if (!likeBtn.disabled) {
-        likes++;
-        document.getElementById("likes").textContent = likes;
-        likeBtn.disabled = true;
-    }
-});
-
-// INPUT FORM
-loginBtn.addEventListener("click", function () {
     let username = usernameInput.value;
     if (username === "") {
         return;
     } else {
-        usernameDisplay.textContent = "Bienvenido/a " + username;
-        usernameInput.style.display = "none";
-        usernameDisplay.style.display = "block";
-    }
-    if (loginBtn.textContent === "Sign In") {
-        addCommentBtn.style.backgroundColor = "blue";
-        addCommentBtn.style.color = "white";
-        loginBtn.textContent = "Sign Out";
-    } else {
-        loginBtn.textContent = "Sign In";
-        usernameInput.value = "";
-        usernameInput.style.display = "block";
-        usernameDisplay.style.display = "none";
+        if (!liked) {
+            likes++;
+            document.getElementById("likes").textContent = likes;
+            liked = true;
+            likeBtn.textContent = "Ya no me gusta";
+            likeBtn.style.color = "red";
+        } else {
+            likes--;
+            document.getElementById("likes").textContent = likes;
+            liked = false;
+            likeBtn.textContent = "Me gusta";
+            likeBtn.style.color = "blue";
+        }
     }
 });
+
 
 // WRITE COMMENTS
 addCommentBtn.addEventListener("click", function () {
     let username = usernameInput.value;
     let comment = commentInput.value;
-    let p = document.createElement("p");
-    let strong = document.createElement("strong");
+    let addComment = document.createElement("p");
+    let addUsername = document.createElement("strong");
+    let deleteCommentBtn = document.createElement("button");
+    let spanBtn = document.createElement("span");
     if (username === "") {
-        return alert("Por favor, ingresa un usuario antes de continuar");
+        return;
     } else {
         if (comment === "") {
-            return alert("Debe escribir algo antes de comentar");
+            return;
         } else {
-            strong.appendChild(document.createTextNode(username));
-            p.appendChild(strong);
-            p.appendChild(document.createTextNode(comment));
-            comments.appendChild(p);
+            deleteCommentBtn.textContent = "Eliminar";
+            deleteCommentBtn.style.backgroundColor = "white";
+            deleteCommentBtn.style.color = "red";
+            deleteCommentBtn.style.fontWeight = "bold";
+            deleteCommentBtn.style.border = "1px solid gray";
+            deleteCommentBtn.style.borderRadius = "3px";
+            spanBtn.style.float = "right";
+            deleteCommentBtn.addEventListener("click", function () {
+                addComment.remove();
+            });
+            spanBtn.appendChild(deleteCommentBtn);
+            addUsername.appendChild(document.createTextNode(username));
+            addComment.appendChild(addUsername);
+            addComment.appendChild(document.createTextNode(" "));
+            addComment.appendChild(document.createTextNode(comment));
+            addComment.appendChild(document.createTextNode(" "));
+            addComment.appendChild(spanBtn);
+            comments.appendChild(addComment);
             commentInput.value = "";
         }
     }
 });
+
